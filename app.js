225ee9463eb745ac9ba64197723e27c412c57f84
Damien Laurent <damsltc57@yahoo.fr>
2025-02-04 23:00:14 +0100

    Fxied error 404

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ const start = async () => {
 	app.use("/history", mailHistory);
 
 	// catch 404 and forward to error handler
-	app.use(function (req, res, next) {
-		next(createError(404));
-	});
+	// app.use(function (req, res, next) {
+	// 	next(createError(404));
+	// });
 
 	// error handler
 	app.use(function (err, req, res, next) {
